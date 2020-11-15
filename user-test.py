a69806e0b6e75a4ba12641ed9d76cc306dd2f408
fiona <fionaniwe@gmail.com>
2020-11-15 12:16:37 +0200

    unittest is impored

---
+++ user-test.py
@@ -0,0 +1,10 @@
+import unittest # Importing the unittest module
+from user import User # Importing the user class
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the User Class behaviours
+    Args:
+        unittest.TestCase : Test case class that helps create test cases
+    '''
+    
