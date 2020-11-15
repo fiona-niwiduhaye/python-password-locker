ca03f41b8339a3de8c276dbb43482cb7baf89dd9
fiona <fionaniwe@gmail.com>
2020-11-15 16:00:50 +0200

    user and credential class is imported

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
-n
+#!/usr/bin/env python3.6
+'''
+
+This is the file that runs the application
+Import User Class from User Module and Credential Class from Credential Module
+'''
+
+from user import User
+from credential import Credential
